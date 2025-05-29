71131f044139c09a0a6fa4aeecb9bfba56241070
omar zinane <zinaneomar2000@gmail.com>
2025-05-29 20:21:12 +0100

    config sign_up function

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -10,23 +10,27 @@ def login():
 def logout():
     return render_template('logout.html')
 
-@auth.route('/sign-up', methods=['GET','POST'])
-def  sign_up():
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
     if request.method == 'POST':
         name = request.form.get('firstname')
         email = request.form.get('email')
         password = request.form.get('password')
-        if len(email)<4:
-            pass
-        elif len(name)<2:
-            pass
-        elif len(password)<7:
-            pass
-        else:
-            #user add
-            pass
+        password2 = request.form.get('password2')
 
-    return render_template('sign_up.html')
+        if not name or not password or not password2:
+            flash('Veuillez remplir tous les champs.', category='error')
+        elif len(name) < 2:
+            flash('Le nom doit contenir au moins 2 caractères.', category='error')
+        elif len(password) < 7:
+            flash('Le mot de passe doit contenir au moins 7 caractères.', category='error')
+        elif password != password2:
+            flash('Les mots de passe ne correspondent pas.', category='error')
+        else:
+            # Ajouter l'utilisateur ici
+            flash('Inscription réussie !', category='success')
+
+    return render_template('sign_up.html')  # ou le nom de ton template
 
     
 
