735124e7365a1d19cb37604a54468b4ef99fe9fd
omar zinane <zinaneomar2000@gmail.com>
2025-06-06 01:43:31 +0100

    create note section with logique for add and delete and update function

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
-from  flask import Blueprint, flash, render_template, request
+from datetime import date
+from  flask import Blueprint, flash, jsonify, redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 from website.models import Note
 from . import db
+import json
+
 views = Blueprint('views',__name__)
 @views.route('/', methods=['GET','POST'])
 @login_required
@@ -17,4 +20,20 @@ def home():
             flash('Note ajoutée avec succès !', category='success')
 
     return render_template('home.html', user= current_user)
+
+@views.route('/delete-note/<int:note_id>', methods=['POST'])
+@login_required
+def delete_note(note_id):
+    note = Note.query.get(note_id)
+    if note and note.user_id == current_user.id:
+        db.session.delete(note)
+        db.session.commit()
+        flash("Note supprimée avec succès.", category='success')
+    else:
+        flash("Suppression non autorisée.", category='error')
+    return redirect(url_for('views.home'))
+
+
+
+    
     
